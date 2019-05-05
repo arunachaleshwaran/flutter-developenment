0d54d6b707525070e7db669a0b7d68f24c102e99
arun <arunachaleshwaran12168@gmail.com>
2019-05-05 23:25:35 +0530

    program 4[05:05:19 23:35] Gesture Detection

--- lib/main.dart
+++ lib/main.dart
@@ -32,10 +32,34 @@ class TutorialHome extends StatelessWidget {
       body: Center(
         child: Text('Hello World'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        tooltip: 'Add',
-        child: Icon(Icons.add),
+      floatingActionButton: MyButton(),
+    );
+  }
+}
+
+class MyButton extends StatefulWidget {
+  @override
+  _MyButtonState createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        print('MyButtion was tapped');
+      },
+      highlightColor:Colors.amber,
+      child: Container(
+        height: 36,
+        padding: EdgeInsets.all(8),
+        margin: EdgeInsets.symmetric(horizontal: 8),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: Colors.lightGreen[500]),
+        child: Center(
+          child: Text('Engage'),
+        ),
       ),
     );
   }
